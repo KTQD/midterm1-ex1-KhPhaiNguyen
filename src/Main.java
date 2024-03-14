97a9304cae5d01b4b1992249a06d1fcb4d582db5
Nguyễn Hoàng Nguyên <129246590+KhPhaiNguyen@users.noreply.github.com>
2024-03-14 07:50:39 +0700

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,10 @@
 public class Main {
     public static void main(String[] args) {
 
-        MP3 mp3;
+        MP3 mp3 = new MP3("chung ta cua hien tai", "nhac son tung", "112233", 100, "8/3/2024");
         mp3.showInfo();
 
-        Book book;
+        Book book = new Book("dac nhan tam", "la sach...", "115588", 50, "Hoang Nguyen", 30, "tam ly");
         book.showInfo();
 
     }
