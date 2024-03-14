2f7fee5de50400d8a7f2d0b63aec1fe2d96ee16b
Nguyễn Hoàng Nguyên <129246590+KhPhaiNguyen@users.noreply.github.com>
2024-03-14 07:51:38 +0700

    Update Item.java

--- src/Item.java
+++ src/Item.java
@@ -1,2 +1,24 @@
-public class Item {
+public abstract class Item {
+    public String name;
+    private String description;
+    protected String ID;
+    int price;
+
+    Item(String name, String description, String ID, int price) {
+        this.name = name;
+        this.description = description;
+        this.ID = ID;
+        this.price = price;
+    }
+
+    void showInfo() {
+        System.out.println("Name: " + name + "\nDescription: " + description + "\nID: " + ID + "\nPrice: " + price);
+    }
+
+    public String getDescription() {
+        return description;
+    }
+    public void setDescription(String description) {
+        this.description = description;
+    }
 }
