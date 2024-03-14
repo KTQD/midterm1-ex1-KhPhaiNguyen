7ab4d997893ffc22fd4b2b06943d2e59da719174
Nguyễn Hoàng Nguyên <129246590+KhPhaiNguyen@users.noreply.github.com>
2024-03-14 07:51:56 +0700

    Update Book.java

--- src/Book.java
+++ src/Book.java
@@ -1,2 +1,17 @@
-public class Book {
+public class Book extends Item {
+    public String author;
+    public int numberOfPages;
+    public String genre;
+
+    Book(String name, String description, String ID, int price, String author, int numberOfPages, String genre) {
+        super(name, description, ID, price);
+        this.author = author;
+        this.numberOfPages = numberOfPages;
+        this.genre = genre;
+    }
+
+    void showInfo() {
+        System.out.println("Name: " + name + "\nDescription: " + getDescription() + "\nID: " + ID + "\nPrice: " + price
+            + "\nAuthor: " + author + "\nNumber of pages: " + numberOfPages + "\nGenre: " + genre);
+    }
 }
